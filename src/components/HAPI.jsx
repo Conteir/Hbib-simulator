912579c05deee15bd377012cffdc749d84a7eddd
Eirik Hafver Rønjum <64041578+eirikconteir@users.noreply.github.com>
2021-06-13 21:44:43 +0200

    Styling CDS v0.1

--- src/components/HAPI.jsx
+++ src/components/HAPI.jsx
@@ -4,17 +4,15 @@ import "../index.css";
 import DisordersAutosuggest from "./DisordersAutosuggest";
 import { HTMLRender } from "./htmlRenderComponent";
 import { codeSystemEnv, params } from "../config.ts";
-import { Spinner } from 'reactstrap';
-
-
+import { Spinner } from "reactstrap";
 
 export const HAPI = class Record extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      env: '',
-      data: '',
+      env: "",
+      data: "",
       matches: -1,
       showContent: false,
       showSpinner: false,
@@ -22,89 +20,95 @@ export const HAPI = class Record extends React.Component {
   }
 
   codeSystemPromise = (url) => {
-    let promise = fetch(url, 
-      params)
-    .then((response) => response.json());
+    let promise = fetch(url, params).then((response) => response.json());
     return promise;
   };
 
   //getting forel and barn link data (h.p.)
   getLinkData = (link) => {
-      let promise = fetch(link.href, params
-    ).then(response => response.json())
-    .then(data => {
-      link.$title = data.tittel;
-    });
+    let promise = fetch(link.href, params)
+      .then((response) => response.json())
+      .then((data) => {
+        link.$title = data.tittel;
+      });
     return promise;
-  }
+  };
 
   //response: handling and processing (h.p.)
   processResponse = (data) => {
     console.log("Processing response:", data);
-    if(!data) return;
-    
+    if (!data) return;
+
     //for links
     let promises = [];
 
     //Preprocess -> get barn and forelder links titles (h.p)
     if (Array.isArray(data)) {
-      data.forEach(item => {
+      data.forEach((item) => {
         if (Array.isArray(item.links)) {
           // object, going through all links
-          item.links.forEach(link => {
-            if (link.rel === 'barn' || link.rel === 'forelder' || link.rel === 'root') {
+          item.links.forEach((link) => {
+            if (
+              link.rel === "barn" ||
+              link.rel === "forelder" ||
+              link.rel === "root"
+            ) {
               promises.push(
-                  // will be pushed after getLinkData finished
-                  this.getLinkData(link)
-                );
+                // will be pushed after getLinkData finished
+                this.getLinkData(link)
+              );
             }
           });
         }
       });
-      
+
       // Text render demo (commented out now) START
-      if(data[0].tekst) {
-        this.setState({content: data[0].tekst});
+      if (data[0].tekst) {
+        this.setState({ content: data[0].tekst });
       }
       // Text render demo (commented out now) END
-    
     } else {
       if (Array.isArray(data.links)) {
         // object, going through all links
-        data.links.forEach(link => {
-          if (link.rel === 'barn' || link.rel === 'forelder' || link.rel === 'root') {
+        data.links.forEach((link) => {
+          if (
+            link.rel === "barn" ||
+            link.rel === "forelder" ||
+            link.rel === "root"
+          ) {
             promises.push(
-                // will be pushed after getLinkData finished
-                this.getLinkData(link)
-              );
+              // will be pushed after getLinkData finished
+              this.getLinkData(link)
+            );
           }
         });
       }
     }
 
     Promise.all(promises).then(() => {
-      this.setState({data: JSON.stringify(data),
-        showSpinner: false
-      });
+      this.setState({ data: JSON.stringify(data), showSpinner: false });
     });
-  }
+  };
 
   // callback to hdir
   linkCallback = (url) => {
-    this.setState({ data: '', showSpinner: true });
+    this.setState({ data: "", showSpinner: true });
     fetch(url, params)
-    .then(response => response.json())
-    .then(data => {
-      this.processResponse(data);
-    }, () => this.setState({ showSpinner: false }));
-  }
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          this.processResponse(data);
+        },
+        () => this.setState({ showSpinner: false })
+      );
+  };
 
   fetchContent = (suggestion) => {
-    this.setState({showSpinner: true});
+    this.setState({ showSpinner: true });
     // reset state to clean results before new loading
-    this.setState({matches: -1, data: '', showContent: false});
+    this.setState({ matches: -1, data: "", showContent: false });
     // API key depends on environment: current -> Production
-    if(!suggestion.$codeSystemResult) return;
+    if (!suggestion.$codeSystemResult) return;
 
     const codeSystemResult = suggestion.$codeSystemResult;
     const codeSystem = codeSystemResult.codeSystem;
@@ -113,20 +117,24 @@ export const HAPI = class Record extends React.Component {
     const url = hdBaseUrl + "?kodeverk=" + codeSystem + "&kode=" + code;
 
     fetch(url, params)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log("Content for " + codeSystem + ":", data);
-        if(Array.isArray(data)) {
-          this.setState({matches: data.length, showSpinner: false});
+        if (Array.isArray(data)) {
+          this.setState({ matches: data.length, showSpinner: false });
         }
         if (Array.isArray(data) && data.length > 0 && data[0].tekst) {
-          this.setState({content: data[0].tekst, data: JSON.stringify(data), showSpinner: false});
+          this.setState({
+            content: data[0].tekst,
+            data: JSON.stringify(data),
+            showSpinner: false,
+          });
 
           console.log("Content for " + codeSystem + ":", data);
           console.log("Content for " + codeSystem + ":", data.length);
         }
 
-         this.processResponse(data);
+        this.processResponse(data);
       });
   };
 
@@ -233,38 +241,39 @@ export const HAPI = class Record extends React.Component {
     return (
       <div>
         <div className="jumbotron text-center">
-            <h1>HAPI and patients record</h1>
-            <p>Choose the code system and make a search throught SNOMED</p>
+          <h1>HAPI and patients record</h1>
+          <p>Choose the code system and make a search throught SNOMED</p>
         </div>
 
         <div className="row, top">
           <div className="col-sm-2">
             <div className="form-group">
               <select
-                  name="codeSystemEnv"
-                  id="codeSystemEnv"
-                  onChange={evt => this.setState({env: evt.target.value})}
+                name="codeSystemEnv"
+                id="codeSystemEnv"
+                onChange={(evt) => this.setState({ env: evt.target.value })}
               >
-                <option value=""
-                        select="default">
-                          Choose target code system
+                <option value="" select="default">
+                  Choose target code system
                 </option>
-                  {/* Rend  er options dynamically from codeSystemEnv */}
-                  {codeSystemEnv.map((codeSystem, key) => 
-                    <option key={key} value={codeSystem.id}>{codeSystem.title}</option>) }
+                {/* Rend  er options dynamically from codeSystemEnv */}
+                {codeSystemEnv.map((codeSystem, key) => (
+                  <option key={key} value={codeSystem.id}>
+                    {codeSystem.title}
+                  </option>
+                ))}
               </select>
-
             </div>
           </div>
         </div>
 
-
         <div className="row">
           <div className="col-sm-6">
-
             <div className="row">
               <div className="form-group">
-                <label htmlFor="notat"><b>Notat:</b></label>
+                <label htmlFor="notat">
+                  <b>Notat:</b>
+                </label>
                 <textarea
                   aria-label="Notat"
                   id="notat"
@@ -277,19 +286,23 @@ export const HAPI = class Record extends React.Component {
 
             <div className="row">
               <div className="form-group">
-                <label htmlFor="funn"><b>Funn:</b></label>
+                <label htmlFor="funn">
+                  <b>Funn:</b>
+                </label>
                 <textarea
                   id="funn"
                   type="text"
                   autoComplete="off"
                   placeholder="funn"
-                  />
+                />
               </div>
             </div>
 
             <div className="row">
               <div className="form-group">
-                <label htmlFor="vurdering"><b>Vurdering:</b></label>
+                <label htmlFor="vurdering">
+                  <b>Vurdering:</b>
+                </label>
                 <textarea
                   id="vurdering"
                   type="text"
@@ -301,7 +314,9 @@ export const HAPI = class Record extends React.Component {
 
             <div className="row">
               <div className="form-group">
-                <label htmlFor="tiltak"><b>Tiltak:</b></label>
+                <label htmlFor="tiltak">
+                  <b>Tiltak:</b>
+                </label>
                 <textarea
                   id="tiltak"
                   type="text"
@@ -310,49 +325,71 @@ export const HAPI = class Record extends React.Component {
                 />
               </div>
             </div>
-
           </div>
 
           <div className="col-sm-6">
             <div className="row">
-              <p><b>Årsak (symptom, plage eller tentativ diagnose):</b></p>
-            </div>
-            
-       
-            <div className="row">
-              <div className="col-sm-8">
-                <DisordersAutosuggest suggestCallback={this.fetchContent} codeSystem={this.state.env}/>
-              </div>
-              
-              <div className="col-sm-4 match-block">
-                {this.state.matches > 0 ?
-                    <div>
-                      <span onClick={() => {this.setState({showContent: true})}} className="badge badge-primary"> {this.state.matches} </span>
-                    </div>
-                    : (this.state.matches === 0 ? <div>No content matches this code</div> : null)
-                  } 
-              </div>
-                  
+              <p>
+                <b>Årsak (symptom, plage eller tentativ diagnose):</b>
+              </p>
             </div>
 
             <div className="row">
-                {this.state.showSpinner ? <Spinner color="success" /> : null}
+              <div className="col-sm-8">
+                <DisordersAutosuggest
+                  suggestCallback={this.fetchContent}
+                  codeSystem={this.state.env}
+                />
+              </div>
+
+              <div className="col-sm-4 match-block">
+                {this.state.matches > 0 ? (
+                  <div>
+                    <span
+                      onClick={() => {
+                        this.setState({ showContent: true });
+                      }}
+                      className="badge badge-primary"
+                    >
+                      {" "}
+                      {this.state.matches}{" "}
+                    </span>
+                  </div>
+                ) : this.state.matches === 0 ? (
+                  <div>No content matches this code</div>
+                ) : null}
+              </div>
             </div>
-            
+
+            <div className="row">
+              {this.state.showSpinner ? <Spinner color="success" /> : null}
+            </div>
+
             <div className="row">
               <div className="col-sm-8">
                 {/* this.state.showContent ? <HTMLRender data={this.state.data} linkCallback={this.linkCallback} /> : null */}
-                {this.state.showContent ? 
-                      <div id="popup-hapi" className="popupHAPI">
-                        <div className="header">
-                          <span>Innhold</span>
-                          <span className="popup-close" onClick={() => this.setState({showContent: false})}>X</span>
-                        </div>
-                        <div className="content">
-                          <HTMLRender data={this.state.data} linkCallback={this.linkCallback} hideMetadata={true} hideLinksNavigation={true} /> {/** --> hide metadata */}
-                        </div>
-                      </div>
-                    : null}
+                {this.state.showContent ? (
+                  <div id="popup-hapi" className="popupHAPI">
+                    <div className="header">
+                      <span>Beslutningsstøtte</span>
+                      <span
+                        className="popup-close"
+                        onClick={() => this.setState({ showContent: false })}
+                      >
+                        X
+                      </span>
+                    </div>
+                    <div className="content">
+                      <HTMLRender
+                        data={this.state.data}
+                        linkCallback={this.linkCallback}
+                        hideMetadata={true}
+                        hideLinksNavigation={true}
+                      />{" "}
+                      {/** --> hide metadata */}
+                    </div>
+                  </div>
+                ) : null}
               </div>
             </div>
           </div>
