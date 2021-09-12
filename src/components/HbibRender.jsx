47ada1c161facd6bd4222d0360bfe700236cb3a6
Hanna <igerne94@gmail.com>
2021-09-12 19:12:07 +0200

    rendering

--- src/components/HbibRender.jsx
+++ src/components/HbibRender.jsx
@@ -11,9 +11,14 @@ export const HbibRender = class HbibRender extends React.Component {
 
   renderJson() {
     console.log("HBibRendering: ", this.props.hbibData);
-    
+
     if(!this.props.hbibData) return <></>;
     let data = JSON.parse(this.props.hbibData);
+
+    // let dataForXAsJson = this.props.hbibData;
+
+
+    // console.log("what is inside xAsJson? ", data?.data?.guillotine?.query.xAsJson._id);
 
     return (
       <div>
@@ -22,11 +27,31 @@ export const HbibRender = class HbibRender extends React.Component {
                     console.log(item);
                     return (
                         <div key={index}>
+                            <p><b>DATA FROM dataAsJson</b></p>
+                            <div dangerouslySetInnerHTML={{ __html: item.dataAsJson.activeIngredient }}></div>
+                            <b><div dangerouslySetInnerHTML={{ __html: item.dataAsJson.title }}></div></b>
                             <div dangerouslySetInnerHTML={{ __html: item.dataAsJson.text }}></div>
+
+                            <p><b>DATA FROM xAsJson</b></p>
+                            <div>{item.xAsJson._id}</div>
                         </div>
                     );
                 }) : null}
             </div>
+
+            {/* <div>
+                {Array.isArray(dataForXAsJson?.data?.guillotine?.query) ? dataForXAsJson.data.guillotine.query.map((item, index) => {
+                    console.log(item);
+                    return (
+                        <div key={index}>
+                            <p><b>DATA FROM xAsJson</b></p>
+                            <div>{item.xAsJson}</div>
+                        </div>
+                    );
+                }) : null}
+            </div> */}
+            
+            
       </div>
     );
   }
