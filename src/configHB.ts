9b5fc395c28c83eaf9ca163e10629a48581352dd
Hanna <igerne94@gmail.com>
2021-09-15 15:55:16 +0200

    clean-up

--- src/configHB.ts
+++ src/configHB.ts
@@ -19,33 +19,6 @@ export const codeSystems = [
     },
   ]
 
-  export const enviroments = [
-    {
-      id: "test-bt",
-      title: "Test BT",
-      url: "https://helsedirektoratet-bt.azure-api.net/innhold/",
-      key: "143e448642124111b8c5e4d7e00d1975",
-    },
-    {
-      id: "test-st",
-      title: "Test ST",
-      url: "https://helsedirektoratet-st.azure-api.net/innhold/",
-      key: "663f23169cae4c8785ec48defa793c23",
-    },
-    {
-      id: "qa",
-      title: "QA",
-      url: "https://helsedirektoratet-at.azure-api.net/innhold/",
-      key: "4f044f75aeea46529ad158cf910e800c",
-    },
-    {
-      id: "prod",
-      title: "Production",
-      url: "https://api.helsedirektoratet.no/innhold/",
-      key: "89b72a3ad5cf4723b3f489c3eb4d82a1",
-    },
-  ]
-
   //SNOMED CT CONFIGURATIONS
   export let terminlogyServer: string = 'https://snowstorm.conteir.no';
   export let branchICD10: string = 'MAIN/SNOMEDCT-NO';
@@ -81,8 +54,6 @@ export const codeSystems = [
       "Ocp-Apim-Subscription-Key": "89b72a3ad5cf4723b3f489c3eb4d82a1",
     },
   };
-
-  // export const helsedirBaseUrl = "https://api.helsedirektoratet.no/innhold/innhold/";
 
 //// configurations for hbib
 // export const hbibUrl = 'https://qa.hbib.ntf.seeds.no/_/service/com.enonic.app.guillotine/graphql';
