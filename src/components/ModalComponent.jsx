4014c4709dbf2733d7a94036accd9cca411b4fd7
Hanna <igerne94@gmail.com>
2022-01-25 21:15:05 +0100

    fetch fix

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -21,7 +21,10 @@ export const ModalComponent = class ModalComponent extends React.Component {
 
         // if proxy sucessful and there are no more issues then this consol log should be printed:
         fetch(fatUrl, params)
-          .then((response) => console.log("Check response", response));
+          .then((response) => response.json())
+          .then((fatData) => {
+              console.log("Check fatData", fatData);      
+          });
       }
     });
   };
