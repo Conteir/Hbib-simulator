4816dfe24633b957354ae313f9199fc910c184e8
Eirik Hafver Rønjum <64041578+eirikconteir@users.noreply.github.com>
2022-01-25 19:58:27 +0100

    Proxy final bug fix

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../index.css";
-import proxyFat from "../config.ts";
+import { proxyFat } from "../config.ts";
 
 export const ModalComponent = class ModalComponent extends React.Component {
   getFatData = () => {
